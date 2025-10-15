docterling/srs

commit 20f6cd595cc8be63a824d29d1332abd4488685be

AI: Refine RTMP/SRT/RTC bridge. v7.0.90 (#4503)
This PR refactors the stream bridge architecture in SRS to improve code
organization, type safety, and maintainability by replacing the generic
ISrsStreamBridge interface with protocol-specific bridge classes and
target interfaces.

1. New Target Interface Architecture:

- Introduces  ISrsFrameTarget for AV frame consumers (RTMP sources)
- Introduces  ISrsRtpTarget for RTP packet consumers (RTC sources)
- Introduces ISrsSrtTarget for SRT packet consumers (SRT sources)

2. Protocol-Specific Bridge Classes:

- SrsRtmpBridge: Converts RTMP frames to RTC/RTSP protocols
-  SrsSrtBridge: Converts SRT packets to RTMP/RTC protocols
-  SrsRtcBridge: Converts RTC packets to RTMP protocol

3. Simplified Bridge Management:

- Removes the generic SrsCompositeBridge chain pattern
- Each source type now uses its appropriate bridge type directly

With this improvement, you are able to implement very complex bridge and
protocol converting, for example, you can bridge RTMP to RTC with opus
audio when you support enhanced RTMP with opus.

Another plan is to support bridging RTC to RTSP, directly without
converting RTP to media frame packet, but directly deliver RTP packet
from RTC source to RTSP source.

---------

Co-authored-by: OSSRS-AI <winlinam@gmail.com>

trunk/src/app/srs_app_rtc_conn.cpp
@@ -1205,8 +1205,11 @@ srs_error_t SrsRtcPublishStream::initialize(ISrsRequest *r, SrsRtcSourceDescript
     source_->set_publish_stream(this);
 
     // TODO: FIMXE: Check it in SrsRtcConnection::add_publisher?
-    SrsSharedPtr<SrsLiveSource> live_source = _srs_sources->fetch(r);
-    if (live_source.get() && !live_source->can_publish(false)) {
+    SrsSharedPtr<SrsLiveSource> live_source;
+    if ((err = _srs_sources->fetch_or_create(r, live_source)) != srs_success) {
+        return srs_error_wrap(err, "create live source");
+    }
+    if (!live_source->can_publish(false)) {
         return srs_error_new(ERROR_SYSTEM_STREAM_BUSY, "rtmp stream %s busy", r->get_stream_url().c_str());
     }
 
@@ -1224,29 +1227,34 @@ srs_error_t SrsRtcPublishStream::initialize(ISrsRequest *r, SrsRtcSourceDescript
         }
     }
 
-    // Bridge to rtmp
-#if defined(SRS_FFMPEG_FIT)
+    // Create the bridge for RTC.
+    SrsRtcBridge *bridge = new SrsRtcBridge();
+
+    // Bridge to RTMP.
+    // TODO: Support bridge to RTSP.
     bool rtc_to_rtmp = _srs_config->get_rtc_to_rtmp(req_->vhost_);
     if (rtc_to_rtmp) {
-        if ((err = _srs_sources->fetch_or_create(r, live_source)) != srs_success) {
-            return srs_error_wrap(err, "create source");
-        }
-
         // Disable GOP cache for RTC2RTMP bridge, to keep the streams in sync,
         // especially for stream merging.
         live_source->set_cache(false);
 
-        SrsCompositeBridge *bridge = new SrsCompositeBridge();
-        bridge->append(new SrsFrameToRtmpBridge(live_source));
-
-        if ((err = bridge->initialize(r)) != srs_success) {
-            srs_freep(bridge);
-            return srs_error_wrap(err, "create bridge");
-        }
-
-        source_->set_bridge(bridge);
+        // Convert RTC to RTMP.
+        bridge->enable_rtc2rtmp(live_source);
     }
-#endif
+
+    if (bridge->empty()) {
+        srs_freep(bridge);
+    } else if ((err = bridge->initialize(r)) != srs_success) {
+        srs_freep(bridge);
+        return srs_error_wrap(err, "create bridge");
+    }
+
+    if ((err = bridge->initialize(r)) != srs_success) {
+        srs_freep(bridge);
+        return srs_error_wrap(err, "create bridge");
+    }
+
+    source_->set_bridge(bridge);
 
     return err;
 }

trunk/src/core/srs_core_version7.hpp
@@ -9,6 +9,6 @@
 
 #define VERSION_MAJOR 7
 #define VERSION_MINOR 0
-#define VERSION_REVISION 89
+#define VERSION_REVISION 90
 
 #endif

trunk/src/app/srs_app_rtc_source.cpp
@@ -396,10 +396,7 @@ SrsRtcSource::SrsRtcSource()
     stream_desc_ = NULL;
 
     req_ = NULL;
-    bridge_ = NULL;
-#ifdef SRS_FFMPEG_FIT
-    frame_builder_ = NULL;
-#endif
+    rtc_bridge_ = NULL;
     circuit_breaker_ = _srs_circuit_breaker;
 
     pli_for_rtmp_ = pli_elapsed_ = 0;
@@ -412,10 +409,7 @@ SrsRtcSource::~SrsRtcSource()
     // for all consumers are auto free.
     consumers_.clear();
 
-#ifdef SRS_FFMPEG_FIT
-    srs_freep(frame_builder_);
-#endif
-    srs_freep(bridge_);
+    srs_freep(rtc_bridge_);
     srs_freep(req_);
     srs_freep(stream_desc_);
 
@@ -591,15 +585,10 @@ SrsContextId SrsRtcSource::pre_source_id()
     return _pre_source_id;
 }
 
-void SrsRtcSource::set_bridge(ISrsStreamBridge *bridge)
+void SrsRtcSource::set_bridge(ISrsRtcBridge *bridge)
 {
-    srs_freep(bridge_);
-    bridge_ = bridge;
-
-#ifdef SRS_FFMPEG_FIT
-    srs_freep(frame_builder_);
-    frame_builder_ = new SrsRtcFrameBuilder(bridge);
-#endif
+    srs_freep(rtc_bridge_);
+    rtc_bridge_ = bridge;
 }
 
 srs_error_t SrsRtcSource::create_consumer(ISrsRtcConsumer *&consumer)
@@ -678,31 +667,30 @@ srs_error_t SrsRtcSource::on_publish()
         return srs_error_wrap(err, "source id change");
     }
 
+    // Setup the audio and video codec.
+    SrsAudioCodecId audio_codec = SrsAudioCodecIdOpus;
+    if (stream_desc_->audio_track_desc_ && stream_desc_->audio_track_desc_->media_) {
+        audio_codec = SrsAudioCodecId(stream_desc_->audio_track_desc_->media_->codec(false));
+    }
+
+    SrsVideoCodecId video_codec = SrsVideoCodecIdAVC;
+    if (stream_desc_->video_track_descs_.size() > 0) {
+        SrsRtcTrackDescription *track_desc = stream_desc_->video_track_descs_.at(0);
+        video_codec = SrsVideoCodecId(track_desc->media_->codec(true));
+    }
+
     // If bridge to other source, handle event and start timer to request PLI.
-    if (bridge_) {
-#ifdef SRS_FFMPEG_FIT
-        SrsAudioCodecId audio_codec = SrsAudioCodecIdOpus;
-        if (stream_desc_->audio_track_desc_ && stream_desc_->audio_track_desc_->media_) {
-            audio_codec = SrsAudioCodecId(stream_desc_->audio_track_desc_->media_->codec(false));
+    if (rtc_bridge_) {
+        if ((err = rtc_bridge_->initialize(req_)) != srs_success) {
+            return srs_error_wrap(err, "rtp bridge initialize");
         }
 
-        SrsVideoCodecId video_codec = SrsVideoCodecIdAVC;
-        if (stream_desc_->video_track_descs_.size() > 0) {
-            SrsRtcTrackDescription *track_desc = stream_desc_->video_track_descs_.at(0);
-            video_codec = SrsVideoCodecId(track_desc->media_->codec(true));
+        if ((err = rtc_bridge_->setup_codec(audio_codec, video_codec)) != srs_success) {
+            return srs_error_wrap(err, "rtp bridge setup codec");
         }
 
-        if ((err = frame_builder_->initialize(req_, audio_codec, video_codec)) != srs_success) {
-            return srs_error_wrap(err, "frame builder initialize");
-        }
-
-        if ((err = frame_builder_->on_publish()) != srs_success) {
-            return srs_error_wrap(err, "frame builder on publish");
-        }
-#endif
-
-        if ((err = bridge_->on_publish()) != srs_success) {
-            return srs_error_wrap(err, "bridge on publish");
+        if ((err = rtc_bridge_->on_publish()) != srs_success) {
+            return srs_error_wrap(err, "rtp bridge on publish");
         }
 
         // The PLI interval for RTC2RTMP.
@@ -741,17 +729,12 @@ void SrsRtcSource::on_unpublish()
     }
 
     // free bridge resource
-    if (bridge_) {
+    if (rtc_bridge_) {
         // For SrsRtcSource::on_timer()
         _srs_shared_timer->timer100ms()->unsubscribe(this);
 
-#ifdef SRS_FFMPEG_FIT
-        frame_builder_->on_unpublish();
-        srs_freep(frame_builder_);
-#endif
-
-        bridge_->on_unpublish();
-        srs_freep(bridge_);
+        rtc_bridge_->on_unpublish();
+        srs_freep(rtc_bridge_);
     }
 
     SrsStatistic *stat = SrsStatistic::instance();
@@ -806,11 +789,9 @@ srs_error_t SrsRtcSource::on_rtp(SrsRtpPacket *pkt)
         }
     }
 
-#ifdef SRS_FFMPEG_FIT
-    if (frame_builder_ && (err = frame_builder_->on_rtp(pkt)) != srs_success) {
-        return srs_error_wrap(err, "frame builder consume packet");
+    if (rtc_bridge_ && (err = rtc_bridge_->on_rtp(pkt)) != srs_success) {
+        return srs_error_wrap(err, "rtp bridge consume packet");
     }
-#endif
 
     return err;
 }
@@ -894,9 +875,9 @@ srs_error_t SrsRtcSource::on_timer(srs_utime_t interval)
 
 #ifdef SRS_FFMPEG_FIT
 
-SrsRtcRtpBuilder::SrsRtcRtpBuilder(SrsFrameToRtcBridge *bridge, SrsSharedPtr<SrsRtcSource> source)
+SrsRtcRtpBuilder::SrsRtcRtpBuilder(ISrsRtpTarget *target, SrsSharedPtr<SrsRtcSource> source)
 {
-    bridge_ = bridge;
+    rtp_target_ = target;
     source_ = source;
 
     req_ = NULL;
@@ -1155,7 +1136,7 @@ srs_error_t SrsRtcRtpBuilder::transcode(SrsParsedAudioPacket *audio)
             break;
         }
 
-        if ((err = bridge_->on_rtp(pkt.get())) != srs_success) {
+        if ((err = rtp_target_->on_rtp(pkt.get())) != srs_success) {
             err = srs_error_wrap(err, "consume opus");
             break;
         }
@@ -1247,7 +1228,7 @@ srs_error_t SrsRtcRtpBuilder::on_video(SrsMediaPacket *msg)
             return srs_error_wrap(err, "package stap-a");
         }
 
-        if ((err = bridge_->on_rtp(pkt.get())) != srs_success) {
+        if ((err = rtp_target_->on_rtp(pkt.get())) != srs_success) {
             return srs_error_wrap(err, "consume sps/pps");
         }
     }
@@ -1383,7 +1364,7 @@ srs_error_t SrsRtcRtpBuilder::consume_packets(vector<SrsRtpPacket *> &pkts)
     // TODO: FIXME: Consume a range of packets.
     for (int i = 0; i < (int)pkts.size(); i++) {
         SrsRtpPacket *pkt = pkts[i];
-        if ((err = bridge_->on_rtp(pkt)) != srs_success) {
+        if ((err = rtp_target_->on_rtp(pkt)) != srs_success) {
             err = srs_error_wrap(err, "consume sps/pps");
             break;
         }
@@ -1758,9 +1739,9 @@ void SrsRtcFrameBuilderAudioPacketCache::clear_all()
     audio_buffer_.clear();
 }
 
-SrsRtcFrameBuilder::SrsRtcFrameBuilder(ISrsStreamBridge *bridge)
+SrsRtcFrameBuilder::SrsRtcFrameBuilder(ISrsFrameTarget *target)
 {
-    bridge_ = bridge;
+    frame_target_ = target;
     is_first_audio_ = true;
     audio_transcoder_ = NULL;
     video_codec_ = SrsVideoCodecIdAVC;
@@ -1892,7 +1873,7 @@ srs_error_t SrsRtcFrameBuilder::transcode_audio(SrsRtpPacket *pkt)
         SrsMediaPacket msg;
         out_rtmp.to_msg(&msg);
 
-        if ((err = bridge_->on_frame(&msg)) != srs_success) {
+        if ((err = frame_target_->on_frame(&msg)) != srs_success) {
             return srs_error_wrap(err, "source on audio");
         }
 
@@ -1922,7 +1903,7 @@ srs_error_t SrsRtcFrameBuilder::transcode_audio(SrsRtpPacket *pkt)
         SrsMediaPacket msg;
         out_rtmp.to_msg(&msg);
 
-        if ((err = bridge_->on_frame(&msg)) != srs_success) {
+        if ((err = frame_target_->on_frame(&msg)) != srs_success) {
             err = srs_error_wrap(err, "source on audio");
             break;
         }
@@ -2099,7 +2080,7 @@ srs_error_t SrsRtcFrameBuilder::do_packet_sequence_header_avc(SrsRtpPacket *pkt,
     SrsMediaPacket msg;
     rtmp.to_msg(&msg);
 
-    if ((err = bridge_->on_frame(&msg)) != srs_success) {
+    if ((err = frame_target_->on_frame(&msg)) != srs_success) {
         return err;
     }
 
@@ -2196,7 +2177,7 @@ srs_error_t SrsRtcFrameBuilder::do_packet_sequence_header_hevc(SrsRtpPacket *pkt
     SrsMediaPacket msg;
     rtmp.to_msg(&msg);
 
-    if ((err = bridge_->on_frame(&msg)) != srs_success) {
+    if ((err = frame_target_->on_frame(&msg)) != srs_success) {
         return err;
     }
 
@@ -2452,7 +2433,7 @@ srs_error_t SrsRtcFrameBuilder::packet_video_rtmp(const uint16_t start, const ui
     SrsMediaPacket msg;
     rtmp.to_msg(&msg);
 
-    if ((err = bridge_->on_frame(&msg)) != srs_success) {
+    if ((err = frame_target_->on_frame(&msg)) != srs_success) {
         srs_warn("fail to pack video frame: %s", srs_error_summary(err).c_str());
         srs_freep(err);
     }

trunk/src/app/srs_app_rtc_source.hpp
@@ -29,7 +29,6 @@ class SrsMediaPacket;
 class SrsRtmpCommonMessage;
 class SrsMessageArray;
 class SrsRtcSource;
-class SrsFrameToRtcBridge;
 class SrsAudioTranscoder;
 class SrsRtpPacket;
 class SrsNaluSample;
@@ -60,6 +59,7 @@ const int SLIDING_WINDOW_SIZE = 10;
 // Maximum waiting time for out-of-order packets (in ms)
 const int MAX_AUDIO_WAIT_MS = 100;
 
+// NTP time for RTC.
 class SrsNtp
 {
 public:
@@ -157,6 +157,7 @@ public:
     void on_stream_change(SrsRtcSourceDescription *desc);
 };
 
+// The RTC source manager.
 class SrsRtcSourceManager : public ISrsHourGlass
 {
 private:
@@ -203,6 +204,7 @@ public:
     virtual const SrsContextId &context_id() = 0;
 };
 
+// The event handler for RTC source.
 class ISrsRtcSourceEventHandler
 {
 public:
@@ -217,8 +219,12 @@ public:
 };
 
 // A Source is a stream, to publish and to play with, binding to SrsRtcPublishStream and SrsRtcPlayStream.
-class SrsRtcSource : public ISrsFastTimer, public ISrsRtcSourceForConsumer
+class SrsRtcSource : public ISrsRtpTarget, public ISrsFastTimer, public ISrsRtcSourceForConsumer
 {
+private:
+    // The RTP bridge, convert RTP packets to other protocols.
+    ISrsRtcBridge *rtc_bridge_;
+
 private:
     // Circuit breaker for protecting server resources.
     ISrsCircuitBreaker *circuit_breaker_;
@@ -233,14 +239,6 @@ private:
     ISrsRtcPublishStream *publish_stream_;
     // Steam description for this steam.
     SrsRtcSourceDescription *stream_desc_;
-
-private:
-#ifdef SRS_FFMPEG_FIT
-    // Collect and build WebRTC RTP packets to AV frames.
-    SrsRtcFrameBuilder *frame_builder_;
-#endif
-    // The Source bridge, bridge stream to other source.
-    ISrsStreamBridge *bridge_;
 
 private:
     // To delivery stream to clients.
@@ -289,7 +287,7 @@ public:
     virtual SrsContextId pre_source_id();
 
 public:
-    void set_bridge(ISrsStreamBridge *bridge);
+    virtual void set_bridge(ISrsRtcBridge *bridge);
 
 public:
     // Create consumer
@@ -313,19 +311,19 @@ public:
 
 public:
     // For event handler
-    void subscribe(ISrsRtcSourceEventHandler *h);
-    void unsubscribe(ISrsRtcSourceEventHandler *h);
+    virtual void subscribe(ISrsRtcSourceEventHandler *h);
+    virtual void unsubscribe(ISrsRtcSourceEventHandler *h);
 
 public:
     // Get and set the publisher, passed to consumer to process requests such as PLI.
-    ISrsRtcPublishStream *publish_stream();
-    void set_publish_stream(ISrsRtcPublishStream *v);
+    virtual ISrsRtcPublishStream *publish_stream();
+    virtual void set_publish_stream(ISrsRtcPublishStream *v);
     // Consume the shared RTP packet, user must free it.
-    srs_error_t on_rtp(SrsRtpPacket *pkt);
+    virtual srs_error_t on_rtp(SrsRtpPacket *pkt);
     // Set and get stream description for source
-    bool has_stream_desc();
-    void set_stream_desc(SrsRtcSourceDescription *stream_desc);
-    std::vector<SrsRtcTrackDescription *> get_track_desc(std::string type, std::string media_type);
+    virtual bool has_stream_desc();
+    virtual void set_stream_desc(SrsRtcSourceDescription *stream_desc);
+    virtual std::vector<SrsRtcTrackDescription *> get_track_desc(std::string type, std::string media_type);
     // interface ISrsFastTimer
 private:
     srs_error_t on_timer(srs_utime_t interval);
@@ -338,7 +336,7 @@ class SrsRtcRtpBuilder
 {
 private:
     ISrsRequest *req_;
-    SrsFrameToRtcBridge *bridge_;
+    ISrsRtpTarget *rtp_target_;
     // The format, codec information.
     SrsRtmpFormat *format_;
     // The metadata cache.
@@ -365,7 +363,7 @@ private:
     bool video_initialized_;
 
 public:
-    SrsRtcRtpBuilder(SrsFrameToRtcBridge *bridge, SrsSharedPtr<SrsRtcSource> source);
+    SrsRtcRtpBuilder(ISrsRtpTarget *target, SrsSharedPtr<SrsRtcSource> source);
     virtual ~SrsRtcRtpBuilder();
 
 private:
@@ -494,7 +492,7 @@ public:
 class SrsRtcFrameBuilder
 {
 private:
-    ISrsStreamBridge *bridge_;
+    ISrsFrameTarget *frame_target_;
 
 private:
     bool is_first_audio_;
@@ -517,7 +515,7 @@ private:
     SrsRtpPacket *obs_whip_pps_;
 
 public:
-    SrsRtcFrameBuilder(ISrsStreamBridge *bridge);
+    SrsRtcFrameBuilder(ISrsFrameTarget *target);
     virtual ~SrsRtcFrameBuilder();
 
 public:

trunk/src/app/srs_app_rtmp_conn.cpp
@@ -1059,17 +1059,17 @@ srs_error_t SrsRtmpConn::acquire_publish(SrsSharedPtr<SrsLiveSource> source)
 
     // Bridge to RTC streaming.
     // TODO: FIXME: Need to convert RTMP to SRT.
-    SrsCompositeBridge *bridge = new SrsCompositeBridge();
+    SrsRtmpBridge *bridge = new SrsRtmpBridge();
 
 #if defined(SRS_FFMPEG_FIT)
     if (rtc.get() && _srs_config->get_rtc_from_rtmp(req->vhost_)) {
-        bridge->append(new SrsFrameToRtcBridge(rtc));
+        bridge->enable_rtmp2rtc(rtc);
     }
 #endif
 
 #ifdef SRS_RTSP
     if (rtsp.get() && _srs_config->get_rtsp_from_rtmp(req->vhost_)) {
-        bridge->append(new SrsFrameToRtspBridge(rtsp));
+        bridge->enable_rtmp2rtsp(rtsp);
     }
 #endif
 

trunk/src/app/srs_app_rtmp_source.cpp
@@ -1706,7 +1706,7 @@ SrsLiveSource::SrsLiveSource()
     stream_die_at_ = 0;
     publisher_idle_at_ = 0;
 
-    bridge_ = NULL;
+    rtmp_bridge_ = NULL;
 
     play_edge_ = new SrsPlayEdge();
     publish_edge_ = new SrsPublishEdge();
@@ -1740,7 +1740,7 @@ SrsLiveSource::~SrsLiveSource()
     srs_freep(gop_cache_);
 
     srs_freep(req_);
-    srs_freep(bridge_);
+    srs_freep(rtmp_bridge_);
 
     SrsContextId cid = _source_id;
     if (cid.empty())
@@ -1852,10 +1852,10 @@ srs_error_t SrsLiveSource::initialize(SrsSharedPtr<SrsLiveSource> wrapper, ISrsR
     return err;
 }
 
-void SrsLiveSource::set_bridge(ISrsStreamBridge *v)
+void SrsLiveSource::set_bridge(ISrsRtmpBridge *v)
 {
-    srs_freep(bridge_);
-    bridge_ = v;
+    srs_freep(rtmp_bridge_);
+    rtmp_bridge_ = v;
 }
 
 srs_error_t SrsLiveSource::on_source_id_changed(SrsContextId id)
@@ -2049,7 +2049,7 @@ srs_error_t SrsLiveSource::on_audio_imp(SrsMediaPacket *msg)
     }
 
     // For bridge to consume the message.
-    if (bridge_ && (err = bridge_->on_frame(msg)) != srs_success) {
+    if (rtmp_bridge_ && (err = rtmp_bridge_->on_frame(msg)) != srs_success) {
         return srs_error_wrap(err, "bridge consume audio");
     }
 
@@ -2170,7 +2170,7 @@ srs_error_t SrsLiveSource::on_video_imp(SrsMediaPacket *msg)
     }
 
     // For bridge to consume the message.
-    if (bridge_ && (err = bridge_->on_frame(msg)) != srs_success) {
+    if (rtmp_bridge_ && (err = rtmp_bridge_->on_frame(msg)) != srs_success) {
         return srs_error_wrap(err, "bridge consume video");
     }
 
@@ -2332,7 +2332,7 @@ srs_error_t SrsLiveSource::on_publish()
         return srs_error_wrap(err, "handle publish");
     }
 
-    if (bridge_ && (err = bridge_->on_publish()) != srs_success) {
+    if (rtmp_bridge_ && (err = rtmp_bridge_->on_publish()) != srs_success) {
         return srs_error_wrap(err, "bridge publish");
     }
 
@@ -2383,9 +2383,9 @@ void SrsLiveSource::on_unpublish()
 
     handler->on_unpublish(req_);
 
-    if (bridge_) {
-        bridge_->on_unpublish();
-        srs_freep(bridge_);
+    if (rtmp_bridge_) {
+        rtmp_bridge_->on_unpublish();
+        srs_freep(rtmp_bridge_);
     }
 
     // no consumer, stream is die.

trunk/src/app/srs_app_rtmp_source.hpp
@@ -507,7 +507,7 @@ public:
 extern SrsLiveSourceManager *_srs_sources;
 
 // The live streaming source.
-class SrsLiveSource : public ISrsReloadHandler
+class SrsLiveSource : public ISrsReloadHandler, public ISrsFrameTarget
 {
     friend class SrsOriginHub;
 
@@ -539,7 +539,7 @@ private:
     // The time of the packet we just got.
     int64_t last_packet_time_;
     // The source bridge for other source.
-    ISrsStreamBridge *bridge_;
+    ISrsRtmpBridge *rtmp_bridge_;
     // The edge control service
     SrsPlayEdge *play_edge_;
     SrsPublishEdge *publish_edge_;
@@ -576,7 +576,7 @@ public:
     // Initialize the hls with handlers.
     virtual srs_error_t initialize(SrsSharedPtr<SrsLiveSource> wrapper, ISrsRequest *r);
     // Bridge to other source, forward packets to it.
-    void set_bridge(ISrsStreamBridge *v);
+    void set_bridge(ISrsRtmpBridge *v);
 
 public:
     // The source id changed.

trunk/src/app/srs_app_rtsp_source.cpp
@@ -503,9 +503,9 @@ void SrsRtspSource::set_video_desc(SrsRtcTrackDescription *video_desc)
     video_desc_ = video_desc->copy();
 }
 
-SrsRtspRtpBuilder::SrsRtspRtpBuilder(SrsFrameToRtspBridge *bridge, SrsSharedPtr<SrsRtspSource> source)
+SrsRtspRtpBuilder::SrsRtspRtpBuilder(ISrsRtpTarget *target, SrsSharedPtr<SrsRtspSource> source)
 {
-    bridge_ = bridge;
+    rtp_target_ = target;
     source_ = source;
 
     req_ = NULL;
@@ -746,7 +746,7 @@ srs_error_t SrsRtspRtpBuilder::on_audio(SrsMediaPacket *msg)
         return srs_error_new(ERROR_NOT_IMPLEMENTED, "codec %d not implemented", acodec);
     }
 
-    if ((err = bridge_->on_rtp(pkt.get())) != srs_success) {
+    if ((err = rtp_target_->on_rtp(pkt.get())) != srs_success) {
         return srs_error_wrap(err, "consume audio packet");
     }
 
@@ -876,7 +876,7 @@ srs_error_t SrsRtspRtpBuilder::on_video(SrsMediaPacket *msg)
             return srs_error_wrap(err, "package stap-a");
         }
 
-        if ((err = bridge_->on_rtp(pkt.get())) != srs_success) {
+        if ((err = rtp_target_->on_rtp(pkt.get())) != srs_success) {
             return srs_error_wrap(err, "consume sps/pps");
         }
     }
@@ -975,7 +975,7 @@ srs_error_t SrsRtspRtpBuilder::consume_packets(vector<SrsRtpPacket *> &pkts)
     // TODO: FIXME: Consume a range of packets.
     for (int i = 0; i < (int)pkts.size(); i++) {
         SrsRtpPacket *pkt = pkts[i];
-        if ((err = bridge_->on_rtp(pkt)) != srs_success) {
+        if ((err = rtp_target_->on_rtp(pkt)) != srs_success) {
             err = srs_error_wrap(err, "consume sps/pps");
             break;
         }

commit 6f526284a3370a5c6c9dd8ccedf2e6bb8fac3ec4

RTC2RTMP: fix illegal memory access. v7.0.97 (#4520)
Regression since 20f6cd595cc8be63a824d29d1332abd4488685be

The early code might meet bridge is empty when
there is no bridge(e.x. rtc to rtc). Then srs_freep will free the brige.

Remove this code that seems redundant.

---------

Co-authored-by: Jacob Su <suzp1984@gmail.com>
Signed-off-by: Jack Lau <jacklau1222@qq.com>

trunk/src/app/srs_app_rtc_conn.cpp
@@ -1310,11 +1310,6 @@ srs_error_t SrsRtcPublishStream::initialize(ISrsRequest *r, SrsRtcSourceDescript
         return srs_error_wrap(err, "create bridge");
     }
 
-    if ((err = bridge->initialize(r)) != srs_success) {
-        srs_freep(bridge);
-        return srs_error_wrap(err, "create bridge");
-    }
-
     source_->set_bridge(bridge);
 
     return err;

trunk/src/core/srs_core_version7.hpp
@@ -9,6 +9,6 @@
 
 #define VERSION_MAJOR 7
 #define VERSION_MINOR 0
-#define VERSION_REVISION 96
+#define VERSION_REVISION 97
 
 #endif

trunk/src/utest/srs_utest_app17.cpp
@@ -4556,12 +4556,12 @@ VOID TEST(CircuitBreakerTest, InitializeAndWaterLevelTransitions)
 
     // Configure circuit breaker settings
     mock_config->circuit_breaker_enabled_ = true;
-    mock_config->high_threshold_ = 90;      // CPU > 90% triggers high water level
-    mock_config->high_pulse_ = 2;           // High level lasts for 2 timer ticks
-    mock_config->critical_threshold_ = 95;  // CPU > 95% triggers critical water level
-    mock_config->critical_pulse_ = 1;       // Critical level lasts for 1 timer tick
-    mock_config->dying_threshold_ = 99;     // CPU > 99% triggers dying water level
-    mock_config->dying_pulse_ = 5;          // Dying level requires 5 consecutive ticks
+    mock_config->high_threshold_ = 90;     // CPU > 90% triggers high water level
+    mock_config->high_pulse_ = 2;          // High level lasts for 2 timer ticks
+    mock_config->critical_threshold_ = 95; // CPU > 95% triggers critical water level
+    mock_config->critical_pulse_ = 1;      // Critical level lasts for 1 timer tick
+    mock_config->dying_threshold_ = 99;    // CPU > 99% triggers dying water level
+    mock_config->dying_pulse_ = 5;         // Dying level requires 5 consecutive ticks
 
     // Create SrsCircuitBreaker
     SrsUniquePtr<SrsCircuitBreaker> breaker(new SrsCircuitBreaker());
@@ -4584,7 +4584,7 @@ VOID TEST(CircuitBreakerTest, InitializeAndWaterLevelTransitions)
     EXPECT_FALSE(breaker->hybrid_dying_water_level());
 
     // Test 3: Simulate high CPU load (91% > high_threshold 90%)
-    mock_host->proc_stat_->percent_ = 0.91;  // 91% CPU
+    mock_host->proc_stat_->percent_ = 0.91; // 91% CPU
     HELPER_EXPECT_SUCCESS(breaker->on_timer(1 * SRS_UTIME_SECONDS));
 
     // After 1 tick with high CPU, high water level should be active
@@ -4593,7 +4593,7 @@ VOID TEST(CircuitBreakerTest, InitializeAndWaterLevelTransitions)
     EXPECT_FALSE(breaker->hybrid_dying_water_level());
 
     // Test 4: Simulate critical CPU load (96% > critical_threshold 95%)
-    mock_host->proc_stat_->percent_ = 0.96;  // 96% CPU
+    mock_host->proc_stat_->percent_ = 0.96; // 96% CPU
     HELPER_EXPECT_SUCCESS(breaker->on_timer(1 * SRS_UTIME_SECONDS));
 
     // After 1 tick with critical CPU, both high and critical should be active
@@ -4603,7 +4603,7 @@ VOID TEST(CircuitBreakerTest, InitializeAndWaterLevelTransitions)
 
     // Test 5: Simulate dying CPU load (99.5% > dying_threshold 99%)
     // Need 5 consecutive ticks to activate dying level
-    mock_host->proc_stat_->percent_ = 0.995;  // 99.5% CPU
+    mock_host->proc_stat_->percent_ = 0.995; // 99.5% CPU
     for (int i = 0; i < 5; i++) {
         HELPER_EXPECT_SUCCESS(breaker->on_timer(1 * SRS_UTIME_SECONDS));
     }
@@ -4614,7 +4614,7 @@ VOID TEST(CircuitBreakerTest, InitializeAndWaterLevelTransitions)
     EXPECT_TRUE(breaker->hybrid_dying_water_level());
 
     // Test 6: Simulate CPU load decrease (back to 50%)
-    mock_host->proc_stat_->percent_ = 0.50;  // 50% CPU
+    mock_host->proc_stat_->percent_ = 0.50; // 50% CPU
     HELPER_EXPECT_SUCCESS(breaker->on_timer(1 * SRS_UTIME_SECONDS));
 
     // Dying level should immediately reset to 0 when CPU drops
@@ -4627,7 +4627,7 @@ VOID TEST(CircuitBreakerTest, InitializeAndWaterLevelTransitions)
 
     // Test 7: Continue with low CPU - high should decay to 0 after 1 more tick
     HELPER_EXPECT_SUCCESS(breaker->on_timer(1 * SRS_UTIME_SECONDS));
-    EXPECT_FALSE(breaker->hybrid_high_water_level());  // High now false (high_water_level_ = 0)
+    EXPECT_FALSE(breaker->hybrid_high_water_level()); // High now false (high_water_level_ = 0)
     EXPECT_FALSE(breaker->hybrid_critical_water_level());
     EXPECT_FALSE(breaker->hybrid_dying_water_level());
 